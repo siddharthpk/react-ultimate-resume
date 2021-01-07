6c0557075d653807321848d9643239bb48556038
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-01-07 08:17:44 -0800

    Added details

--- src/package/components/cards/cards_types/studies/studies_front/studies_front.jsx
+++ src/package/components/cards/cards_types/studies/studies_front/studies_front.jsx
@@ -58,7 +58,7 @@ const Content = ({ hasEducation, data, handleAddButtonClick, classes }) => {
                 <ProfileCardFrontTypography classes={{ container: classes.typography }}>
                     <FormattedMessage
                         id="Studies.title"
-                        defaultMessage="I graduated from {schoolName}"
+                        defaultMessage="I'm currently attending the {schoolName}"
                         values={{ schoolName: data?.[0]?.institution }}
                     />
                 </ProfileCardFrontTypography>
